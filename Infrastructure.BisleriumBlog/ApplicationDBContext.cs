047dd2fc3a631fccb3ea14d171904b94bc496bf2
MonkeyareDragon <githubforme73@gmail.com>
2024-04-05 23:25:49 +0545

    Create a sql connection through ApplicationDBContext class.

---
+++ Infrastructure.BisleriumBlog/ApplicationDBContext.cs
@@ -0,0 +1,19 @@
+﻿using Domain.BisleriumBlog;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.BisleriumBlog
+{
+    public class ApplicationDBContext : DbContext 
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-6G1QAIB;Database=BisleriumBloggers;Trusted_Connection=True;TrustServerCertificate=True;MultipleActiveResultSets=True");
+        }
+        public DbSet<Post> Posts { get; set; }
+    }
+}
