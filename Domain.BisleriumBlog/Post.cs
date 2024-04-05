5e4da701eb7c4de1cff6f406851c73d6f9556a7b
Kabin Chongbang Limbu <kcl0038@my.londonmet.ac.uk>
2024-04-05 23:12:48 +0545

    Add Relationship among layers and Create the Post model.

---
+++ Domain.BisleriumBlog/Post.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.BisleriumBlog
+{
+    public class Post
+    {
+        public int PostId { get; set; }
+        public string? Title { get; set; }
+        public string? Content { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+        public string? ImageUrl { get; set; }
+    }
+}
